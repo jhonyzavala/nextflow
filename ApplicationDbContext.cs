3e093a62d4e652e434e81f442fac7d5e3bdad2d5
Jhony Zavala Vazquez <jhony.zavala@gmail.com>
2023-10-24 14:22:18 -0600

    Security implementation start

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using webapi_nextflow.Entity;
 
 namespace webapi_nextflow
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
--- Migrations/20231024200859_system-user.Designer.cs
+++ Migrations/20231024200859_system-user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapi_nextflow;
 
@@ -11,9 +12,11 @@ using webapi_nextflow;
 namespace webapi_nextflow.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024200859_system-user")]
+    partial class systemuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
